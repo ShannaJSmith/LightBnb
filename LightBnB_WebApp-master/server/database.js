1b3f887fcae013ef8de8b756cf733ac57ed385a4
ShannaJSmith <shanna_smith987@hotmail.com>
2021-10-14 20:45:38 +0000

    updated getAllProperties. May need further editing

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -100,23 +100,93 @@ exports.getAllReservations = getAllReservations;
 
 /**
  * Get all properties.
- * @param {{}} options An object containing query options.
+ * @param {{}} options An object containing query options. Options allows users to filter their results. 
+ * An options object can potentially have the following properties:
+  {city, owner_id, minimum_price_per_night, maximum_price_per_night, minimum_rating;} <- input slots on search page 
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows); //prints the select statement
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log('ERROR:', err.message); //prints terminal unexpectedly ended comment
-    });
-};
-exports.getAllProperties = getAllProperties;
 
+ const getAllProperties = function (options, limit = 10) {
+  // 1 Setup an array to hold any parameters that may be available for the query.
+  const queryParams = [];
+  // 2 Start the query with all information that comes before the WHERE clause (from property by city query).
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  /* 3 Check if a city has been passed in as an option. Add the city to the params array and create a WHERE clause for the city.
+We can use the length of the array to dynamically get the $n placeholder number. Since this is the first parameter, it will be $1.
+The % syntax for the LIKE clause must be part of the parameter, not the query. */
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    if (!queryString.includes('WHERE')) {
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    } else {
+      queryString += ` AND city LIKE $${queryParams.length} `;
+    }
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    if (!queryString.includes('WHERE')) {
+      queryString += `WHERE owner_id = $${queryParams.length}`;
+    } else {
+      queryString += ` AND owner_id = $${queryParams.length}`;
+    }
+  }
+
+  if (options.minimum_price_per_night) {
+    // clients will input a price in dollars so need to multiple by 100 to get the values in database (which are stored in cents)
+    queryParams.push(options.minimum_price_per_night * 100);
+    if (!queryString.includes('WHERE')) {
+      queryString += ` WHERE cost_per_night > $${queryParams.length}`;
+    } else {
+      queryString += ` AND cost_per_night > $${queryParams.length}`;
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    if (!queryString.includes('WHERE')) {
+      queryString += ` WHERE cost_per_night < $${queryParams.length}`;
+    } else {
+      queryString += ` AND cost_per_night < $${queryParams.length}`;
+    }
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+  // 4 Add any query that comes after the WHERE clause.
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  // 5 Console log everything just to make sure we've done it right.
+  console.log('queryString:', queryString, 'queryParams:', queryParams);
+  // 6 Run the query
+  return pool
+  .query(queryString, queryParams)
+  .then((res) => res.rows);
+};
+
+//  const getAllProperties = (options, limit = 10) => {
+//   return pool
+//     .query(`SELECT * FROM properties LIMIT $1`, [limit])
+//     .then((result) => {
+//       console.log(result.rows); //prints the select statement
+//       return result.rows;
+//     })
+//     .catch((err) => {
+//       console.log('ERROR:', err.message); //prints terminal unexpectedly ended comment
+//     });
+// };
+exports.getAllProperties = getAllProperties;
 
 /**
  * Add a property to the database
